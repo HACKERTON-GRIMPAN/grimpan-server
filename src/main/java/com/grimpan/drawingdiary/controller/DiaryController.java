46b3c4fa28eb64bd0254e854c0dc5d92fcb414bc
hj-k66 <hjx66@naver.com>
2023-06-29 04:06:54 +0900

    feat: 스웨거 설정 & 일기 작성 api 구현

--- src/main/java/com/grimpan/drawingdiary/controller/DiaryController.java
+++ src/main/java/com/grimpan/drawingdiary/controller/DiaryController.java
@@ -1,8 +1,30 @@
 package com.grimpan.drawingdiary.controller;
 
+import com.grimpan.drawingdiary.dto.DiaryWriteRequest;
+import com.grimpan.drawingdiary.dto.DiaryWriteResponse;
+import com.grimpan.drawingdiary.service.DiaryService;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class DiaryController {
 
+@Api(tags = "사례일 전시(캘린더)")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/diary")
+public class DiaryController {
+    private final DiaryService diaryService;
+
+    @Operation(summary = "일기 작성")
+    @PostMapping("")
+    public ResponseEntity<DiaryWriteResponse> createDiary(@ModelAttribute DiaryWriteRequest request) {
+        DiaryWriteResponse response = diaryService.create(request);
+        return ResponseEntity.ok()
+                .body(response);
+    }
 }
