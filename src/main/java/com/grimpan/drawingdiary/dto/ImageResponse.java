1021c4b0439f1d262da766c9fce44f93b14baf89
hj-k66 <hjx66@naver.com>
2023-06-29 15:52:43 +0900

    feat: 일기 작성 시 이미지 생성 & 이미지 선택 api 구현

---
+++ src/main/java/com/grimpan/drawingdiary/dto/ImageResponse.java
@@ -0,0 +1,11 @@
+package com.grimpan.drawingdiary.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ImageResponse {
+    private String imageName;
+    private String imageData;
+}
