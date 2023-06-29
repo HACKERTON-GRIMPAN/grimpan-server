3b4fd636683ec35921e7c661306bd531f6f928af
HyungJoonSon <kr.dev.hyungjoon@gmail.com>
2023-06-29 16:44:14 +0900

    feat: Unit Update And Cors Check

--- src/main/java/com/grimpan/drawingdiary/dto/DiaryWriteRequest.java
+++ src/main/java/com/grimpan/drawingdiary/dto/DiaryWriteRequest.java
@@ -2,9 +2,11 @@ package com.grimpan.drawingdiary.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class DiaryWriteRequest {
     private String title;
     private String content;
